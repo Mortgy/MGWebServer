56ec5f46600234b738248cc0d2889e815aba1e48
Muhammed Mortgy <m@mortgy.com>
2024-05-31 01:58:25 +0300

    Initial Commit

---
+++ MGWebServer/MGWebServer.h
@@ -0,0 +1,18 @@
+//
+//  MGWebServer.h
+//  MGWebServer
+//
+//  Created by Mortgy on 5/31/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MGWebServer.
+FOUNDATION_EXPORT double MGWebServerVersionNumber;
+
+//! Project version string for MGWebServer.
+FOUNDATION_EXPORT const unsigned char MGWebServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MGWebServer/PublicHeader.h>
+
+
